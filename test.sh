b8cf46c3e1e2292229ca4008c7d6c6710977d801
Jason Brown <jrb239@cl.cam.ac.uk>
2022-06-28 18:54:03 +0100

    Test script fix and requirements for gpu stuff

--- test.sh
+++ test.sh
@@ -2,7 +2,7 @@
 
 export PYTHONPATH="$(pwd)":"$PYTHONPATH"
 
-python lra_benchmarks/text_classification/train.py \
+XLA_FLAGS=--xla_gpu_force_compilation_parallelism=1 python lra_benchmarks/text_classification/train.py \
       --config=lra_benchmarks/text_classification/configs/transformer_base_test.py \
       --model_dir=models/text_classification \
       --task_name="yelp_reviews" \
