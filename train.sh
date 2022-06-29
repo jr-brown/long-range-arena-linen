72003f80e5a78ee096826bf11d5d8bfc00f450a0
Jason Brown <jasonrbrown@virginmedia.com>
2022-06-29 17:14:24 +0100

    Fixed model traing dir conflicting with model dir

--- train.sh
+++ train.sh
@@ -4,7 +4,7 @@ export PYTHONPATH="$(pwd)":"$PYTHONPATH"
 
 python -u lra_benchmarks/text_classification/train.py \
       --config=lra_benchmarks/text_classification/configs/transformer_base.py \
-      --model_dir=models/text_classification \
+      --model_dir=trained_models/text_classification \
       --task_name="yelp_reviews" \
       >> train_log.txt 2>&1
 
