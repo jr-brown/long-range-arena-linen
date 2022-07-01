2917244f6afc84e2ac95cbf4ea909c98d0ec91bc
Jason Brown <jasonrbrown@virginmedia.com>
2022-07-01 14:30:05 +0100

    GenericDualEncoder

--- lra_benchmarks/models/longformer/longformer.py
+++ lra_benchmarks/models/longformer/longformer.py
@@ -13,6 +13,7 @@
 # limitations under the License.
 """Longformer modules."""
 from typing import Any
+from functools import partial
 
 from flax import linen as nn
 import jax.numpy as jnp
@@ -155,92 +156,8 @@ class LongformerEncoder(nn.Module):
         return encoded
 
 
-class LongformerDualEncoder(nn.Module):
-    """Longformer Model for Matching (dual encoding) tasks."""
-
-    vocab_size: Any=None
-    use_bfloat16: Any=False
-    emb_dim: Any=512
-    num_heads: Any=8
-    num_layers: Any=6
-    qkv_dim: Any=512
-    mlp_dim: Any=2048
-    max_len: Any=2048
-    dropout_rate: Any=0.1
-    attention_dropout_rate: Any=0.1
-    classifier: Any=True
-    classifier_pool: Any='CLS'
-    num_classes: Any=2
-    interaction: Any=None
-
-    @nn.compact
-    def __call__(self, inputs1, inputs2, *, inputs1_positions=None, inputs2_positions=None,
-                 inputs1_segmentation=None, inputs2_segmentation=None, train: bool=False):
-        """Applies Transformer model on text similarity.
-
-        A deliberate choice to distinguish this from NLI because
-        we may want to do different things to the model later. Dual Encoding
-        mode enforces that we do not do cross attention between pairs.
-
-        Args:
-            inputs1: input data.
-            inputs2: target data.
-            vocab_size: size of the input vocabulary.
-            inputs1_positions: input subsequence positions for packed examples.
-            inputs2_positions: target subsequence positions for packed examples.
-            inputs1_segmentation: input segmentation info for packed examples.
-            inputs2_segmentation: target segmentation info for packed examples.
-            use_bfloat16: bool: whether use bfloat16.
-            emb_dim: dimension of embedding.
-            num_heads: number of heads.
-            num_layers: number of layers.
-            qkv_dim: dimension of the query/key/value.
-            mlp_dim: dimension of the mlp on top of attention block.
-            max_len: maximum length.
-            train: whether it is training.
-            dropout_rate: dropout rate.
-            attention_dropout_rate: dropout rate for attention weights.
-            classifier: boolean, to use classifier.
-            classifier_pool: str, supports "MEAN", "MAX" pooling.
-            num_classes: int, number of classification classes.
-            interaction: str
-
-        Returns:
-            output of a transformer decoder.
-        """
-
-        encoder = LongformerEncoder(
-                vocab_size=self.vocab_size,
-                use_bfloat16=self.use_bfloat16,
-                emb_dim=self.emb_dim,
-                num_heads=self.num_heads,
-                num_layers=self.num_layers,
-                qkv_dim=self.qkv_dim,
-                mlp_dim=self.mlp_dim,
-                max_len=self.max_len,
-                dropout_rate=self.dropout_rate,
-                attention_dropout_rate=self.attention_dropout_rate,
-                name='encoder')
-        inputs1_encoded = encoder(
-                inputs=inputs1,
-                inputs_positions=inputs1_positions,
-                inputs_segmentation=inputs1_segmentation,
-                train=train)
-        inputs2_encoded = encoder(
-                inputs=inputs2,
-                inputs_positions=inputs2_positions,
-                inputs_segmentation=inputs2_segmentation,
-                train=train)
-
-        encoded = common_layers.classifier_head_dual(
-                inputs1_encoded,
-                inputs2_encoded,
-                self.num_classes,
-                self.mlp_dim,
-                pooling_mode=self.classifier_pool,
-                interaction=self.interaction)
-
-        return encoded
+LongformerDualEncoder = partial(generic.GenericDualEncoder,
+                                encoder_module=LongformerEncoder)
 
 
 class LongformerDecoder(nn.Module):
