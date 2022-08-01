c129dcac05bdb65e5e31a917091e085006ef9f60
Jason Brown <jasonrbrown@virginmedia.com>
2022-08-01 10:06:24 +0100

    Fixed some temporary shenanigans

--- lra_benchmarks/models/performer/performer.py
+++ lra_benchmarks/models/performer/performer.py
@@ -1,16 +1,3 @@
-# Copyright 2021 Google LLC
-
-# Licensed under the Apache License, Version 2.0 (the "License");
-# you may not use this file except in compliance with the License.
-# You may obtain a copy of the License at
-
-#     https://www.apache.org/licenses/LICENSE-2.0
-
-# Unless required by applicable law or agreed to in writing, software
-# distributed under the License is distributed on an "AS IS" BASIS,
-# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-# See the License for the specific language governing permissions and
-# limitations under the License.
 """Performer-based language models."""
 from functools import partial
 from typing import Any, Optional
@@ -32,6 +19,46 @@ _ATTENTION_FNS = {
 }
 
 
+class PerformerBlock(nn.Module):
+    """Performer layer (https://arxiv.org/abs/2006.03555)."""
+
+    raw_attention_fn: Any
+    qkv_dim: int
+    mlp_dim: int
+    num_heads: int
+    dtype: Any=jnp.float32
+    dropout_rate: float=0.1
+    attention_dropout_rate: float=0.1
+    max_len: int=512
+    layer_num: int=0
+
+    @nn.compact
+    def __call__(self, inputs, *, inputs_segmentation=None, causal_mask: bool=False,
+                 padding_mask=None, deterministic: bool=False):
+
+        attention_fn = self.raw_attention_fn(self.qkv_dim // self.num_heads,
+                                             unidirectional=causal_mask)
+
+        encoded = generic.GenericBlock(
+            attention_module=attention.MaskedSelfAttention,
+            qkv_dim=self.qkv_dim,
+            mlp_dim=self.mlp_dim,
+            num_heads=self.num_heads,
+            dtype=self.dtype,
+            dropout_rate=self.dropout_rate,
+            attention_dropout_rate=self.attention_dropout_rate,
+            max_len=self.max_len,
+            attention_module_kwargs={"attention_fn": attention_fn},
+        )(
+            inputs,
+            inputs_segmentation=inputs_segmentation,
+            causal_mask=causal_mask,
+            padding_mask=padding_mask,
+            deterministic=deterministic,
+        )
+        return encoded
+
+
 def get_modules(attention_fn_cls: str='generalized',
                 attention_fn_kwargs: Optional[dict[str, Any]]=None) -> ModuleCollection:
 
@@ -43,43 +70,5 @@ def get_modules(attention_fn_cls: str='generalized',
     if attention_fn_kwargs is not None:
         raw_attention_fn = partial(raw_attention_fn, **attention_fn_kwargs)
 
-    class block(nn.Module):
-        """Performer layer (https://arxiv.org/abs/2006.03555)."""
-
-        qkv_dim: int
-        mlp_dim: int
-        num_heads: int
-        dtype: Any=jnp.float32
-        dropout_rate: float=0.1
-        attention_dropout_rate: float=0.1
-        max_len: int=512
-        layer_num: int=0
-
-        @nn.compact
-        def __call__(self, inputs, *, inputs_segmentation=None, causal_mask: bool=False,
-                     padding_mask=None, deterministic: bool=False):
-
-            attention_fn = raw_attention_fn(self.qkv_dim // self.num_heads,
-                                            unidirectional=causal_mask)
-
-            encoded = generic.GenericBlock(
-                attention_module=attention.MaskedSelfAttention,
-                qkv_dim=self.qkv_dim,
-                mlp_dim=self.mlp_dim,
-                num_heads=self.num_heads,
-                dtype=self.dtype,
-                dropout_rate=self.dropout_rate,
-                attention_dropout_rate=self.attention_dropout_rate,
-                max_len=self.max_len,
-                attention_module_kwargs={"attention_fn": attention_fn},
-            )(
-                inputs,
-                inputs_segmentation=inputs_segmentation,
-                causal_mask=causal_mask,
-                padding_mask=padding_mask,
-                deterministic=deterministic,
-            )
-            return encoded
-
-    return ModuleCollection(None, block=block)
+    return ModuleCollection(None, block=partial(PerformerBlock, raw_attention_fn=raw_attention_fn))
 
--- lra_benchmarks/models/sinkhorn_transformer/sinkhorn_transformer.py
+++ lra_benchmarks/models/sinkhorn_transformer/sinkhorn_transformer.py
@@ -10,71 +10,73 @@ from lra_benchmarks.models.generic import generic
 from lra_benchmarks.models.generic.module_collection import ModuleCollection
 
 
+class SinkhornEncoder(nn.Module):
+    """Sinkhorn Transformer Encoder."""
+
+    block: Any
+    vocab_size: Any
+    shared_embedding: Any=None
+    use_bfloat16: bool=False
+    dtype: Any=jnp.float32
+    emb_dim: int=512
+    num_heads: int=8
+    num_layers: int=6
+    qkv_dim: int=512
+    mlp_dim: int=2048
+    max_len: int=512
+    dropout_rate: float=0.1
+    attention_dropout_rate: float=0.1
+    learn_pos_emb: bool=False
+    classifier: bool=False
+    classifier_pool: Any='MEAN'
+    num_classes: int=10
+
+    def setup(self):
+        if self.classifier and self.classifier_pool == 'CLS':
+            self._max_len = self.max_len + 1
+        else:
+            self._max_len = self.max_len
+
+    @nn.compact
+    def __call__(self, inputs, *, inputs_positions=None, inputs_segmentation=None, train=True):
+        def custom_classifier_func(encoded):
+            if self.classifier_pool == 'MEAN':
+                encoded = jnp.mean(encoded, axis=1)
+                return nn.Dense(self.num_classes, name='logits')(encoded)
+            else:
+                # TODO(yitay): Add other pooling methods.
+                raise ValueError(f'{self.classifier_pool} Pooling method not supported yet.')
+
+        x = generic.GenericEncoder(
+            block_module=self.block,
+            vocab_size=self.vocab_size,
+            shared_embedding=self.shared_embedding,
+            use_bfloat16=self.use_bfloat16,
+            dtype=self.dtype,
+            emb_dim=self.emb_dim,
+            num_heads=self.num_heads,
+            num_layers=self.num_layers,
+            qkv_dim=self.qkv_dim,
+            mlp_dim=self.mlp_dim,
+            max_len=self._max_len,
+            dropout_rate=self.dropout_rate,
+            attention_dropout_rate=self.attention_dropout_rate,
+            learn_pos_emb=self.learn_pos_emb,
+            classifier=self.classifier,
+            classifier_pool=self.classifier_pool,
+            num_classes=self.num_classes,
+            custom_classifier_func=custom_classifier_func,
+        )(inputs, inputs_positions=inputs_positions, inputs_segmentation=inputs_segmentation,
+          train=train)
+
+        return x
+
+
 def get_modules(block_size: int=20, max_num_blocks: int=25, sort_activation: str="softmax"
                 ) -> ModuleCollection:
     attn = partial(SinkhornSelfAttention, block_size=block_size, max_num_blocks=max_num_blocks,
                    sort_activation=sort_activation)
     block = partial(generic.GenericBlock, attention_module=attn)
 
-    class encoder(nn.Module):
-        """Sinkhorn Transformer Encoder."""
-
-        vocab_size: Any
-        shared_embedding: Any=None
-        use_bfloat16: bool=False
-        dtype: Any=jnp.float32
-        emb_dim: int=512
-        num_heads: int=8
-        num_layers: int=6
-        qkv_dim: int=512
-        mlp_dim: int=2048
-        max_len: int=512
-        dropout_rate: float=0.1
-        attention_dropout_rate: float=0.1
-        learn_pos_emb: bool=False
-        classifier: bool=False
-        classifier_pool: Any='MEAN'
-        num_classes: int=10
-
-        def setup(self):
-            if self.classifier and self.classifier_pool == 'CLS':
-                self._max_len = self.max_len + 1
-            else:
-                self._max_len = self.max_len
-
-        @nn.compact
-        def __call__(self, inputs, *, inputs_positions=None, inputs_segmentation=None, train=True):
-            def custom_classifier_func(encoded):
-                if self.classifier_pool == 'MEAN':
-                    encoded = jnp.mean(encoded, axis=1)
-                    return nn.Dense(self.num_classes, name='logits')(encoded)
-                else:
-                    # TODO(yitay): Add other pooling methods.
-                    raise ValueError(f'{self.classifier_pool} Pooling method not supported yet.')
-
-            x = generic.GenericEncoder(
-                block_module=block,
-                vocab_size=self.vocab_size,
-                shared_embedding=self.shared_embedding,
-                use_bfloat16=self.use_bfloat16,
-                dtype=self.dtype,
-                emb_dim=self.emb_dim,
-                num_heads=self.num_heads,
-                num_layers=self.num_layers,
-                qkv_dim=self.qkv_dim,
-                mlp_dim=self.mlp_dim,
-                max_len=self._max_len,
-                dropout_rate=self.dropout_rate,
-                attention_dropout_rate=self.attention_dropout_rate,
-                learn_pos_emb=self.learn_pos_emb,
-                classifier=self.classifier,
-                classifier_pool=self.classifier_pool,
-                num_classes=self.num_classes,
-                custom_classifier_func=custom_classifier_func,
-            )(inputs, inputs_positions=inputs_positions, inputs_segmentation=inputs_segmentation,
-              train=train)
-
-            return x
-
-    return ModuleCollection(attn, block=block, encoder=encoder)
+    return ModuleCollection(attn, block=block, encoder=partial(SinkhornEncoder, block=block))
 
